6c02e68f937135054caf028867256f5382d7a4c0
binglu895 <56194545+binglu895@users.noreply.github.com>
2024-05-19 16:04:59 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  match: "mp4",
   maxPagesToCrawl: 5,
   outputFileName: "output.json",
   maxTokens: 2000000,
