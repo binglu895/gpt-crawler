621d91ac5c45f0fbfc8879990891875f563c776a
binglu895 <56194545+binglu895@users.noreply.github.com>
2024-05-19 16:27:41 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,8 +2,8 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://www.gg51-lpfw010.com/",
-  match: "https://www.gg51-lpfw010.com/*.ts",
-  maxPagesToCrawl: 100,
+  match: "https://www.gg51-lpfw010.com/**",
+  maxPagesToCrawl: 500,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
